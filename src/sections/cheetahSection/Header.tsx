a86f1b135f853d5a46d97f9a895522b514f2e3fb
TAHPAPANGKORN <papangkorn.pitjawong.work@gmail.com>
2025-03-27 13:10:45 +0700

    add smooth scroll

--- src/sections/cheetahSection/Header.tsx
+++ src/sections/cheetahSection/Header.tsx
@@ -5,7 +5,7 @@ export const Header = () => {
     <h1 className="hidden md:block font-semibold">Papangkorn Pitjawong</h1>
     <nav className="flex gap-1 p-0.5 border border-black/10 rounded-full bg-white/10 backdrop-blur">
       <a href="#" className="nav-item">Home</a>
-      <a href="#" className="nav-item">About</a>
+      <a href="#about" className="nav-item">About</a>
       <a href="#" className="nav-item">Certificate</a>
       <a href="#" className="nav-item">Projects</a>
       <a href="#" className="nav-item bg-gray-900 text-white hover:bg-black/50 hover:text-white-900">Contacts</a>
