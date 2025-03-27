01b5d8755edce29043e82cbe649bfc5c682d8135
TAHPAPANGKORN <papangkorn.pitjawong.work@gmail.com>
2025-03-28 02:30:28 +0700

    host??

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import { Header } from "@/app/component/Header";
 import { HeroSection } from "@/app/component/Hero";
 import { StackCards }  from "@/app/component/StackCard";
+
+export const runtime = 'edge';
+
 const cheetahPage = () => {
   return (
     <div>
