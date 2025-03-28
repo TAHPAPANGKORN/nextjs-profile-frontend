8ad67adee6e2bf167e5b1a759aea69d25c7af9c8
TAHPAPANGKORN <papangkorn.pitjawong.work@gmail.com>
2025-03-28 17:40:54 +0700

    add Project and redesign stackcard

--- src/app/components/Project.tsx
+++ src/app/components/Project.tsx
@@ -1,10 +1,79 @@
 import React from 'react'
+import Image from 'next/image';
+import DiscordProject  from "@/app/assets/images/Poke-discord.png"
+
 export const Project = () => {
   return (
-    <div id="project" className='h-[100vh] flex items-center justify-center'>
-      <h1 className='text-2xl md:text-6xl text-red-700 underline underline-offset-1'>Project now in development</h1>
+    <div
+      id="project"
+      className="bg-[#F3F4F6] flex flex-col items-center justify-center py-5 gap-20 "
+    >
+      <h1 className="header-item mt-5">Projects</h1>
+      <div className="flex flex-col md:flex-row items-center justify-center space-y-4 md:space-y-0 md:space-x-8 mx-2 md:gap-6">
+        <Image
+          src={DiscordProject}
+          alt="Discord-Project"
+          height={300}
+          width={500}
+          className="rounded-xs shadow shadow-gray-400"
+        />
+        <div className="flex flex-col gap-3 w-full">
+          <h2 className="font-bold text-2xl">Poke bot discord</h2>
+          <p className="text-[12px] md:text-xl ">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Exercitationem, voluptatum.
+          </p>
+          <div className="flex  gap-4 underline underline-offset-1 text-blue-500">
+            <a href="#">View Project</a>
+            <a href="#">GitHub Repo</a>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-col md:flex-row items-center justify-center space-y-4 md:space-y-0 md:space-x-8 mx-2 md:gap-6">
+        <Image
+          src={DiscordProject}
+          alt="Discord-Project"
+          height={300}
+          width={500}
+          className='rounded-xs shadow shadow-gray-400'
+        />
+        <div className='flex flex-col gap-3 w-full'>
+          <h2 className='font-bold text-2xl'>Poke bot discord</h2>
+          <p className='text-[12px] md:text-xl '>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Exercitationem, voluptatum.
+          </p>
+          <div className='flex  gap-4 underline underline-offset-1 text-blue-500'>
+            <a href="#">View Project</a>
+            <a href="#">GitHub Repo</a>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-col md:flex-row items-center justify-center space-y-4 md:space-y-0 md:space-x-8 mx-2 md:gap-6">
+        <Image
+          src={DiscordProject}
+          alt="Discord-Project"
+          height={300}
+          width={500}
+          className='rounded-xs shadow shadow-gray-400'
+        />
+        <div className='flex flex-col gap-3 w-full'>
+          <h2 className='font-bold text-2xl'>Poke bot discord</h2>
+          <p className='text-[12px] md:text-xl '>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Exercitationem, voluptatum.
+          </p>
+          <div className='flex  gap-4 underline underline-offset-1 text-blue-500'>
+            <a href="#">View Project</a>
+            <a href="#">GitHub Repo</a>
+          </div>
+        </div>
+      </div>
+
     </div>
-  )
+  );
 }
 
 export default Project
