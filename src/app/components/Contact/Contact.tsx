d4a2cccd5be7f799129672f9a91c2d0dce593b5c
TAHPAPANGKORN <papangkorn.pitjawong.work@gmail.com>
2025-04-02 14:14:14 +0700

    Change bg color gradient in contact page

--- src/app/components/Contact/Contact.tsx
+++ src/app/components/Contact/Contact.tsx
@@ -3,7 +3,7 @@ import ContactCard from "./ContactCard";
 
 export default function Timeline() {
   return (
-    <div id="contact" className="flex flex-col items-center py-10 w-auto">
+    <div id="contact" className="flex flex-col items-center py-10 w-auto bg-gradient-to-b from-white to-gray-200">
       <h1 className="header-item mt-10">Contact me</h1>
       <ContactCard />
     </div>
