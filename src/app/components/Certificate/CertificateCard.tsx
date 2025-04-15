a842d452cfb014a83f59ac75dc971d1c6cd81c47
TAHPAPANGKORN <papangkorn.pitjawong.work@gmail.com>
2025-04-15 12:01:14 +0700

    feat: add show image feature

--- src/app/components/Certificate/CertificateCard.tsx
+++ src/app/components/Certificate/CertificateCard.tsx
@@ -1,6 +1,8 @@
+"use client";
 import Image, { StaticImageData } from 'next/image'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Link from 'next/link'
+import { div } from 'framer-motion/client';
 
 interface CertificateCardProps {
   imageSrc: string | StaticImageData;
@@ -10,18 +12,65 @@ interface CertificateCardProps {
 }
 
 
+
 const CertificateCard: React.FC<CertificateCardProps> = ({ 
   imageSrc, 
   altText, 
   title, 
   description, 
 }) => {
+  const [showImage, setShowImage] = useState(false);
+
+  const handleViewClick = (e: React.MouseEvent) => {
+    e.preventDefault(); 
+    setShowImage(!showImage); 
+  };
+
+
+  useEffect(() => {
+    if (showImage) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "";
+    }
+
+    return () => {
+      document.body.style.overflow = "";
+    };
+  }, [showImage]);
+  
+
   return (
-    <div className="flex flex-col-reverse md:flex-row items-center justify-center gap-5 space-y-4 md:space-y-0 md:space-x-8 mx-2 md:gap-6">
+    <div className={`flex flex-col-reverse md:flex-row items-center justify-center gap-5 space-y-4 md:space-y-0 md:space-x-8 mx-2 md:gap-6`}>
       <div className="flex flex-col gap-2 w-full">
         <h2 className="font-bold max-w-50 text-2xl">{title}</h2>
-        <p className="text-[12px] w-90 md:text-sm  md:w-50  lg:text-xl lg:w-130" >{description}</p>
-        <Link href="#" className='underline underline-offset-1 text-blue-500 flex md:justify-end hover:text-gray-500'>View Image</Link>
+        <p className="text-[12px] w-90 md:text-sm  md:w-50  lg:text-xl lg:w-130">
+          {description}
+        </p>
+        <Link
+          onClick={handleViewClick}
+          href="#"
+          className="underline underline-offset-1 text-blue-500 flex md:justify-end hover:text-gray-500"
+        >
+          View Image
+        </Link>
+      {showImage && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/90">
+          <button
+            onClick={handleViewClick}
+            className="absolute top-5 right-5 text-white z-50 cursor-pointer"
+          >
+            Close
+          </button>
+          <Image
+            src={imageSrc}
+            alt={altText}
+            height={600}
+            width={800}
+            className="p-4 w-300 rounded-xl"
+          />
+        </div>
+      )}
       </div>
       <Image
         src={imageSrc}
