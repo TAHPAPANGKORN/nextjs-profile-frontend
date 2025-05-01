40c795237df75da3ef30ff3bc4306dacfb857111
TAHPAPANGKORN <papangkorn.pitjawong.work@gmail.com>
2025-05-02 06:09:01 +0700

    styling: fix fade up animation

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -12,33 +12,34 @@ export const HeroSection = () => {
 
   return (
     <div
-      data-aos="fade-up"
       id="home"
       className="relative z-0 overflow-x-clip min-h-screen flex flex-col items-center justify-center bg-gradient-to-t from-white to-gray-200"
-      >
-      <div className="flex flex-col justify-center items-center mx-20 md:mx-40">
-        <h1 className="text-3xl md:text-5xl text-center mt-8 tracking-wide">
-          <TypeAnimation
-            sequence={[
-              "Hi, I am Papangkorn",
-              3000, 
-              "Hi, I am study in com-sci",
-              2000,
-            ]}
-            wrapper="span"
-            speed={30}
-            repeat={Infinity}
-          />
-        </h1>
-        <p className="mt-4 text-center">
-          I am currently a Computer Science major with a strong interest in
-          programming, software development, and problem-solving. I enjoy
-          learning about new technologies and improving my coding skills. My
-          goal is to apply my knowledge to real-world applications and
-          contribute to innovative projects in the tech industry.
-        </p>
+    >
+      <div data-aos="fade-up">
+        <div className="flex flex-col justify-center items-center mx-20 md:mx-40">
+          <h1 className="text-3xl md:text-5xl text-center mt-8 tracking-wide">
+            <TypeAnimation
+              sequence={[
+                "Hi, I am Papangkorn",
+                3000,
+                "Hi, I am study in com-sci",
+                2000,
+              ]}
+              wrapper="span"
+              speed={30}
+              repeat={Infinity}
+            />
+          </h1>
+          <p className="mt-4 text-center">
+            I am currently a Computer Science major with a strong interest in
+            programming, software development, and problem-solving. I enjoy
+            learning about new technologies and improving my coding skills. My
+            goal is to apply my knowledge to real-world applications and
+            contribute to innovative projects in the tech industry.
+          </p>
+        </div>
+        <BtnHero />
       </div>
-      <BtnHero />
     </div>
   );
 }
